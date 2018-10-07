333da0e3d1c259c824b5f226b6ee18f5c3839dc6
Julien Viet <julien@julienviet.com>
2018-10-07 17:23:48 +0200

    Revert "More log"
    This reverts commit 9af4873c06cc8e191e6adf3a6560377b44c664bc.

--- src/test/java/io/vertx/ext/shell/command/base/BusTest.java
+++ src/test/java/io/vertx/ext/shell/command/base/BusTest.java
@@ -327,7 +327,7 @@ public class BusTest {
   }
 
   private void assertBusTail(TestContext context, String cmd, Runnable send, Predicate<String> check) {
-    System.out.println("start test <" + cmd + ">");
+    System.out.println("start test");
     Async runningLatch = context.async();
     Shell shell = server.createShell();
     Pty pty = Pty.create();
